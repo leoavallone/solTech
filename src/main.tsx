a55ea6480d56c48dddd467d1b225a60d9f014b5f
Leonardo Enrico Souza Roque Avallone <avallone@MacBook-Air-de-Avallone.local>
2025-07-31 12:50:37 -0300

    Subindo tela de Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import Login from "./login.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
